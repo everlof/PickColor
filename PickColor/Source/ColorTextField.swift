2b7220927b9a41bf8083d8b56dcdf4f4d370d8e0
David Everlöf <everlof@gmail.com>
2018-10-13 08:33:36 +0200

    Prevent that the ColorTextField is becoming to long

--- PickColor/Source/ColorTextField.swift
+++ PickColor/Source/ColorTextField.swift
@@ -46,6 +46,12 @@ class ColorTextField: UITextField, UITextFieldDelegate {
         return false
     }
 
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let currentText = textField.text as NSString? else { return false }
+        let newText = currentText.replacingCharacters(in: range, with: string)
+        return newText.first == "#" ? newText.count <= 7 : newText.count <= 6
+    }
+
     func textFieldDidBeginEditing(_ textField: UITextField) {
         prevText = text
     }
