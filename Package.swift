e9e33ee7b8e8443e374b302215cd01c39a5eca3a
David Everlöf <everlof@gmail.com>
2020-12-01 20:03:16 +0100

    Update platform -> iOS 13

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "PickColor",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         .library(name: "PickColor", targets: ["PickColor"])
     ],
